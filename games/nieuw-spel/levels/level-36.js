e1066d1796714e5556cdc0b50a4a7bb35f3dc93b
BenWestinga <benwestinga1@gmail.com>
2026-09-12 01:10:58 +0200

    Nieuwe game beginnen

--- games/nieuw-spel/levels/level-36.js
+++ games/nieuw-spel/levels/level-36.js
@@ -40,15 +40,16 @@ export const config = {
     spawnGroups: [
 
         { enemy: "snowstorm", count: 1, start: 5, duration: 1},
-        { enemy: "iceGoon", count: 100, start: 5, duration: 30},
+        { enemy: "iceGoon", count: 200, start: 5, duration: 50},
         { enemy: "snowWorm", count: 2, start: 40, duration: 20},
-        { enemy: "snowGuy", count: 15, start: 40, duration: 20},
+        { enemy: "snowGuy", count: 45, start: 40, duration: 60},
         { enemy: "snowHealer", count: 5, start: 75, duration: 100},
         { enemy: "snowWorm", count: 10, start: 75, duration: 65},
         { enemy: "snowstorm", count: 1, start: 100, duration: 1},
         { enemy: "snowstorm", count: 1, start: 160, duration: 1},
         { enemy: "iceGoon", count: 100, start: 120, duration: 30},
         { enemy: "snowWorm", count: 5, start: 140, duration: 1},
+        { enemy: "iceGoon", count: 100, start: 170, duration: 30}
 
     ]
 };
--- games/nieuw-spel/levels/level-37.js
+++ games/nieuw-spel/levels/level-37.js
@@ -39,7 +39,10 @@ export const config = {
 
     spawnGroups: [
 
-        // ENEMIES HIER
+        { enemy: "snowWorm", count: 14, start: 5, duration: 250},
+        { enemy: "snowHealer", count: 6, start: 5, duration: 250},
+        { enemy: "snowProtector", count: 8, start: 5, duration: 250},
+        { enemy: "iceGoon", count: 300, start: 5, duration: 100}
 
     ]
 };
