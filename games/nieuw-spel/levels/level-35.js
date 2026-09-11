6b49c8e95cd4059bd894b348502ea98aa71f383f
BenWestinga <benwestinga1@gmail.com>
2026-09-11 22:03:51 +0200

    Add Ice Tank health bar and splitting ice ball attack

--- games/nieuw-spel/levels/level-35.js
+++ games/nieuw-spel/levels/level-35.js
@@ -54,7 +54,7 @@ export const config = {
         { enemy: "snowHealer", count: 1, start: 160, duration: 30},
         { enemy: "snowHealer", count: 1, start: 240, duration: 30},
         { enemy: "snowMan", count: 8, start: 80, duration: 120},
-        { enemy: "snowGuy", count: 80, start: 80, duration: 120},
+        { enemy: "snowGuy", count: 20, start: 80, duration: 120},
         { enemy: "snowProtector", count: 3, start: 80, duration: 120},
 
         { enemy: "iceTank", count: 1, start: 200, duration: 1}
