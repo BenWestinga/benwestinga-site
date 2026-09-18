6a38f6e8d6580754a3110a065302deb8e164586f
BenWestinga <benwestinga1@gmail.com>
2026-09-18 13:39:06 +0200

    Prevent crash when explosion kills player during bullet hit

---
+++ games/nieuw-spel/levels/level-44.js
@@ -0,0 +1,87 @@
+import lavaGoon from "../enemies/enemy-34.js";
+import meteor from "../enemies/enemy-35.js";
+import lavaWave from "../enemies/enemy-36.js";
+
+import lavaGolem, {
+    lavaGolemite
+} from "../enemies/enemy-37.js";
+
+import lavaWizard from "../enemies/enemy-38.js";
+import plasmaGuy from "../enemies/enemy-39.js";
+import lavaBen from "../enemies/boss-09.js";
+import lavaBurrower from "../enemies/enemy-40.js";
+import emberHealer from "../enemies/enemy-41.js";
+import ashPhantom from "../enemies/enemy-42.js";
+import cinderSplitter, {
+    cinder
+} from "../enemies/enemy-43.js";
+import fireChainTwins from "../enemies/enemy-44.js";
+
+
+
+
+export const config = {
+    number: 44,
+
+    name: "Level 44",
+
+    startDelayMs: 5000,
+
+    background: {
+        image: "lava.png",
+        alpha: 0.58,
+        color: "#9b3827"
+    },
+
+    enemyTypes: {
+        lavaGoon,
+        meteor,
+        lavaWave,
+        lavaGolem,
+        lavaGolemite,
+        lavaWizard,
+        plasmaGuy,
+        lavaBen,
+        lavaBurrower,
+        emberHealer,
+        ashPhantom,
+        cinderSplitter,
+        cinder,
+        fireChainTwins
+    },
+
+    spawnGroups: [
+
+        { enemy: "cinderSplitter", count: 8, start: 5, duration: 25},
+        { enemy: "lavaGolem", count: 4, start: 5, duration: 45},
+        { enemy: "lavaBurrower", count: 12, start: 5, duration: 130},
+        { enemy: "cinderSplitter", count: 8, start: 50, duration: 45},
+        { enemy: "cinderSplitter", count: 8, start: 100, duration: 45},
+        { enemy: "lavaWave", count: 40, start: 5, duration: 125},
+        
+    ]
+};
+
+
+export async function start(
+    context = {}
+) {
+    return window.startStoryLevel(
+        config,
+        context
+    );
+}
+
+
+export async function startLevel(
+    context = {}
+) {
+    return start(context);
+}
+
+
+export default {
+    config,
+    start,
+    startLevel
+};
